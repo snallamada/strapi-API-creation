b2e0fd07db0e30402d3186178dcd0ff889db771e
snallamada <shravanreddynallamada@gmail.com>
2024-01-19 17:03:46 +0100

    database postgres connected.

--- backend/config/database.js
+++ backend/config/database.js
@@ -49,12 +49,12 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
+        connectionString: env('manny.db.elephantsql.com'),
+        host: env('DATABASE_HOST', 'manny.db.elephantsql.com'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        database: env('DATABASE_NAME', 'hiqbuhmg'),
+        user: env('DATABASE_USERNAME', 'hiqbuhmg'),
+        password: env('DATABASE_PASSWORD', 'nPBcnrwnA5HSuKQ3vbI-qqsWu1vs6JMq'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
