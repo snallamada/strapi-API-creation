f6e1fed2d9f21eeb618caf098d59efb4616d72d0
snallamada <shravanreddynallamada@gmail.com>
2024-01-21 19:37:01 +0100

    ADD DB CONFIG WITH  PG

--- backend/config/database.js
+++ backend/config/database.js
@@ -47,29 +47,29 @@ module.exports = ({ env }) => {
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
-    postgres: {
-      connection: {
-        connectionString: env('manny.db.elephantsql.com'),
-        host: env('DATABASE_HOST', 'manny.db.elephantsql.com'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'hiqbuhmg'),
-        user: env('DATABASE_USERNAME', 'hiqbuhmg'),
-        password: env('DATABASE_PASSWORD', 'nPBcnrwnA5HSuKQ3vbI-qqsWu1vs6JMq'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool(
-            'DATABASE_SSL_REJECT_UNAUTHORIZED',
-            true
-          ),
-        },
-        schema: env('DATABASE_SCHEMA', 'public'),
-      },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-    },
+    // postgres: {
+    //   connection: {
+    //     connectionString: env('manny.db.elephantsql.com'),
+    //     host: env('DATABASE_HOST', 'manny.db.elephantsql.com'),
+    //     port: env.int('DATABASE_PORT', 5432),
+    //     database: env('DATABASE_NAME', 'hiqbuhmg'),
+    //     user: env('DATABASE_USERNAME', 'hiqbuhmg'),
+    //     password: env('DATABASE_PASSWORD', 'nPBcnrwnA5HSuKQ3vbI-qqsWu1vs6JMq'),
+    //     ssl: env.bool('DATABASE_SSL', false) && {
+    //       key: env('DATABASE_SSL_KEY', undefined),
+    //       cert: env('DATABASE_SSL_CERT', undefined),
+    //       ca: env('DATABASE_SSL_CA', undefined),
+    //       capath: env('DATABASE_SSL_CAPATH', undefined),
+    //       cipher: env('DATABASE_SSL_CIPHER', undefined),
+    //       rejectUnauthorized: env.bool(
+    //         'DATABASE_SSL_REJECT_UNAUTHORIZED',
+    //         true
+    //       ),
+    //     },
+    //     schema: env('DATABASE_SCHEMA', 'public'),
+    //   },
+    //   pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+    // },
     sqlite: {
       connection: {
         filename: path.join(
